874cbc95afc1fbb9c614859625841f9f534a670a
lucero-cardenas <76122129+lucero-cardenas@users.noreply.github.com>
2021-03-18 05:57:00 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="container">
-      <header>
-        <h1>Weather App</h1>
-      </header>
-      <footer>
-        <p>
-          <a href="https://github.com/lucero-cardenas/weather-react-final-project" target="_blank" rel="noreferrer">Open source code</a>
-          {" "}by{" "}
-          <a href="mailto:lucero.cardenas@gmail.com" target="_blank" rel="noreferrer">Lucero Cardenas</a>
-        </p>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <header>
+          <h1>Weather App</h1>
+        </header>
+        <footer>
+          <p>
+            <a href="https://github.com/lucero-cardenas/weather-react-final-project" target="_blank" rel="noreferrer">Open source code</a>
+            {" "}by{" "}
+            <a href="mailto:lucero.cardenas@gmail.com" target="_blank" rel="noreferrer">Lucero Cardenas</a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
