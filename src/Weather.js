b4606d92bc17c6d521168092caf4d15891b8027c
lucero-cardenas <76122129+lucero-cardenas@users.noreply.github.com>
2021-03-19 04:49:52 +0100

    Added elements and fixed alignment
    made to work for mobile phones and desk computers

--- src/Weather.js
+++ src/Weather.js
@@ -3,13 +3,44 @@ import "./Weather.css";
 import Date from "./Date.js";
 
 export default function Weather () {
-    return<div className= "Weather">
-        <h1 className= "card-title">Country</h1>
-        <ul>
-            <li><h5 className= "card-subtitle"><Date/></h5></li>
-            <li>Humidity:</li>
-            <li>Wind Speed:</li>
-            <li>Precipitation:</li>
-        </ul>
+    return<div className= "Weather card">
+        <div className="row">
+            <form>
+                <input type="text" placeholder="Enter a city"/>
+            </form>
+        </div>
+        <div className="row">
+            <span className="card card-body col-xs-6 col-md-3 w-0">
+                <h1 className= "card-title">Country</h1>
+                <ul>
+                    <li><h5 className= "card-subtitle mb-2 text-muted"><Date/></h5></li>
+                    <li className="card-subtitle mb-2 text-muted">Humidity:</li>
+                    <li className="card-subtitle mb-2 text-muted">Wind Speed:</li>
+                    <li className="card-subtitle mb-2 text-muted">Precipitation:</li>
+                </ul>
+            </span>
+            <span className="card card-body col-xs-6 col-md-3 w-0 icon">
+                <ul>
+                    <li><h2 className="card-title">🌦</h2></li>
+                    <li className="card-subtitle mb-2 text-muted">Description</li>
+                </ul>
+            </span>        
+            <span className="card card-body col-xs-6 col-md-3 w-0 temp">
+                <ul>
+                    <li><h2 className="card-title">-13°</h2></li>
+                    <li className="card-subtitle mb-2 text-muted">Feels like:</li>
+                </ul>
+            </span>
+            <span className="card card-body col-xs-6 col-md-3 w-0">
+                <ul>
+                    <li><h4 className="card-title">C | F</h4></li>
+                    <li className="card-subtitle mb-2">Max:</li>
+                    <li className="card-subtitle mb-2">Min:</li>
+                </ul>
+            </span>
+        </div>
+        <div className="row">
+            <h3>Forecast</h3>
+        </div>
     </div>;
 }
